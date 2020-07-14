4e37db8b0d42677e54317a1ed6aa894f6d66ed46
clydekarnage <clydeibalez@gmail.com>
2020-07-14 13:24:01 +0400

    Request & Body Validation

--- models/User.js
+++ models/User.js
@@ -19,6 +19,10 @@ const UserSchema = new.mongoose.Schema({
   avatar: {
     type: String,
   },
+  age: {
+    type: String,
+    required: true,
+  },
   date: {
     type: Date,
     default: Date.now,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const { response } = require("express");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("username", "Username is required!").not().isEmpty(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+    check("email", "Please include a valid Email").isEmail(),
+    check("age", "Please input a valid age number!").not().isEmpty().isInt(),
+  ],
+  (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
