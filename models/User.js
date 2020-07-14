54e1a3c3c5484358629ca9a0aba13eb24ad498b1
clydekarnage <clydeibalez@gmail.com>
2020-07-14 12:34:01 +0400

    Creating The User Models

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = request("mongoose");
+
+const UserSchema = new.mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
