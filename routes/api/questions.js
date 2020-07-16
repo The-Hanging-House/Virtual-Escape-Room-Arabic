ab4b2edd3d1907b0c22817f374a4225168c4d5f3
clydekarnage <clydeibalez@gmail.com>
2020-07-16 16:36:01 +0400

    Creating The Question Model

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+const Question = require("../../models/Question");
+
 // @route   GET api/questions
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Question route"));
+router.post("/", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const { alternatives } = req.body;
+
+    const question = await Question.create({
+      description,
+      alternatives,
+    });
+
+    return res.status(200).json(question);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
