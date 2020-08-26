ff14bea3a8290459bf5cec7e04fe2a2cef287fba
clydekarnage <clydeibalez@gmail.com>
2020-08-26 14:05:40 +0400

    test

---
+++ client/build/precache-manifest.d4384b258f3236ee2fa15313fc86737b.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe790a26be11d8fced87e018108dbcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80e56dfcd552286f808",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "99ebe5886f7d16d8249c",
+    "url": "/static/css/main.db7b98a5.chunk.css"
+  },
+  {
+    "revision": "f80e56dfcd552286f808",
+    "url": "/static/js/2.16b3c2c8.chunk.js"
+  },
+  {
+    "revision": "822e79ec612b860e70eff5e8fdf551a6",
+    "url": "/static/js/2.16b3c2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ebe5886f7d16d8249c",
+    "url": "/static/js/main.9d66c30b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "57bac92696cbe41f3789e9cda639aac1",
+    "url": "/static/media/01.57bac926.png"
+  },
+  {
+    "revision": "f60776c4d8f515326fa370e7bcb691e3",
+    "url": "/static/media/Birds.f60776c4.mp3"
+  },
+  {
+    "revision": "7e2327fc47602dd69af25ccc057d26bf",
+    "url": "/static/media/Instructions1.7e2327fc.png"
+  },
+  {
+    "revision": "cf975182f83286e427a75622cdca5668",
+    "url": "/static/media/Party.cf975182.mp3"
+  },
+  {
+    "revision": "8b3b90b19bfb2fbdf4e82a01d61ad2fd",
+    "url": "/static/media/answer.8b3b90b1.svg"
+  },
+  {
+    "revision": "f08995c2c59ed4374429b3d753955b4b",
+    "url": "/static/media/bg.f08995c2.png"
+  },
+  {
+    "revision": "5c49681b555ae9584dabd57a01766f04",
+    "url": "/static/media/bgend.5c49681b.png"
+  },
+  {
+    "revision": "489fc71865aee42c2ea8c4695abe6ab4",
+    "url": "/static/media/board.489fc718.jpg"
+  },
+  {
+    "revision": "383c17a9aa6af8a061ae28281c227426",
+    "url": "/static/media/briefing1.383c17a9.mp4"
+  },
+  {
+    "revision": "9d531761dbd57454091512442683a703",
+    "url": "/static/media/briefing2.9d531761.mp4"
+  },
+  {
+    "revision": "914e68157632310ac10a3e4ad733fb5d",
+    "url": "/static/media/component1.914e6815.png"
+  },
+  {
+    "revision": "297d37f9a2a61204bd7786f97b20c9ea",
+    "url": "/static/media/egg.297d37f9.jpg"
+  },
+  {
+    "revision": "a9c2f8433724ff748df60639978b7511",
+    "url": "/static/media/feather.a9c2f843.jpg"
+  },
+  {
+    "revision": "04901adb8751909a70245675cd013d22",
+    "url": "/static/media/image1.04901adb.png"
+  },
+  {
+    "revision": "14a25c3a26f19e62169c91fd830dadad",
+    "url": "/static/media/loader.14a25c3a.gif"
+  },
+  {
+    "revision": "ed9552dec13b1e9c1e2db094ed871015",
+    "url": "/static/media/lock.ed9552de.png"
+  },
+  {
+    "revision": "f84962e4ae9e7e89b45f692d83e213d8",
+    "url": "/static/media/logout.f84962e4.svg"
+  },
+  {
+    "revision": "b9e05f78a6008d55141b9ebd7339874a",
+    "url": "/static/media/ny_01.b9e05f78.png"
+  },
+  {
+    "revision": "0d78f1a9e1e83701c3e73a934750ad17",
+    "url": "/static/media/ny_02.0d78f1a9.png"
+  },
+  {
+    "revision": "15faf56534b2b53efe18059ba982de68",
+    "url": "/static/media/ny_04.15faf565.png"
+  },
+  {
+    "revision": "503907c42d8d2761e86848a430a79d33",
+    "url": "/static/media/ny_05.503907c4.png"
+  },
+  {
+    "revision": "01719b805d67639c0a8b21597b48f1a6",
+    "url": "/static/media/ny_06.01719b80.png"
+  },
+  {
+    "revision": "bb73ceaf2b4be5528ee2097969a3b292",
+    "url": "/static/media/ny_07.bb73ceaf.png"
+  },
+  {
+    "revision": "7225a204011f2a0a21f80ffae3017aa5",
+    "url": "/static/media/ny_08.7225a204.png"
+  },
+  {
+    "revision": "9f086cd7bd3ff07477930854fd3a25cd",
+    "url": "/static/media/ny_10.9f086cd7.png"
+  },
+  {
+    "revision": "fc6981be409a23c97f562b770239af80",
+    "url": "/static/media/ny_11.fc6981be.png"
+  },
+  {
+    "revision": "205b30a15693fa9ed3e6379017dbda0e",
+    "url": "/static/media/ny_15.205b30a1.png"
+  },
+  {
+    "revision": "3c308f5bedebd4c5c8320bf32c057967",
+    "url": "/static/media/ny_16.3c308f5b.png"
+  },
+  {
+    "revision": "ea3b27233019dffbef077ca99d2272a8",
+    "url": "/static/media/ny_20.ea3b2723.png"
+  },
+  {
+    "revision": "37d947a844269f6d52ca30471c11606e",
+    "url": "/static/media/ny_original.37d947a8.jpg"
+  },
+  {
+    "revision": "019f8f92d9a0cf18eef130061f4dda41",
+    "url": "/static/media/plant.019f8f92.jpg"
+  },
+  {
+    "revision": "39b6577809a9adc4824492a659cb1e1a",
+    "url": "/static/media/popup.39b65778.svg"
+  },
+  {
+    "revision": "ff3d160a38ed04f4eaa7323d4a10dab1",
+    "url": "/static/media/pp1.ff3d160a.png"
+  },
+  {
+    "revision": "fc6981be409a23c97f562b770239af80",
+    "url": "/static/media/pp4.fc6981be.png"
+  },
+  {
+    "revision": "8a5afd45e20d8cf60c3bddb1b59b87db",
+    "url": "/static/media/pp5.8a5afd45.png"
+  },
+  {
+    "revision": "e68577f8244c185261981afe63a07ac5",
+    "url": "/static/media/pp6.e68577f8.png"
+  },
+  {
+    "revision": "fbd8e9bb5206ab45dd789429c8eea064",
+    "url": "/static/media/rectangle.fbd8e9bb.svg"
+  },
+  {
+    "revision": "9587eea19a1df3cb1d3eb3030a92f6ff",
+    "url": "/static/media/reject.9587eea1.svg"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/static/media/spinner.26ef8bb4.gif"
+  },
+  {
+    "revision": "5be0450cefa67b95786985013921599c",
+    "url": "/static/media/tools.5be0450c.jpg"
+  },
+  {
+    "revision": "708d8d53c03df563ff5fc31dd3478bb5",
+    "url": "/static/media/trap.708d8d53.jpg"
+  },
+  {
+    "revision": "796a88fa68996155b50dba978cf64a24",
+    "url": "/static/media/trunk1.796a88fa.jpg"
+  },
+  {
+    "revision": "845359c974d5ab58f9e9adf0fb47fbeb",
+    "url": "/static/media/watercooler.845359c9.jpg"
+  }
+]);
