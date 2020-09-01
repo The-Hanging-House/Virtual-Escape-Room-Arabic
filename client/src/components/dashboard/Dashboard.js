309d9bdce5d949d9295842a832b703d3460aeb45
clydekarnage <clydeibalez@gmail.com>
2020-09-01 14:47:26 +0400

    changes3

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,10 @@ import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile'
 import bg from '../../img/01.png'
 
+import Logout from './../logout/Logout'
+
+const logout = <Logout />
+
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
@@ -14,6 +18,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
     // console.log(user && user._id)
     return loading && profile === null ? <Spinner /> : 
     <Fragment>
+        {logout}
         <div className="bg-image"></div>
         <div className="bg-text1"></div>
         <img className="bg-text1" src={bg} alt="Dashboard" useMap="#workmap" width="600" height="420" />
