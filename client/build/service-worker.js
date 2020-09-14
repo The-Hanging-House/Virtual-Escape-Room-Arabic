009ea6ce33639b6f0d18226b0735a04a750667c8
clydekarnage <clydeibalez@gmail.com>
2020-09-14 14:17:22 +0400

    added the sound

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.822a2064fbe539276019b56242daa24d.js"
+  "/precache-manifest.92366992656eca2ab04e464a615974da.js"
 );
 
 self.addEventListener('message', (event) => {
