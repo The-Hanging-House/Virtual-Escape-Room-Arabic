e7fc8071628e03886e09d242fdc1301add56f6f0
clydekarnage <clydeibalez@gmail.com>
2020-09-16 12:27:39 +0400

    some minor changes build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80db4064af71ed5cf6919456064d3fe8.js"
+  "/precache-manifest.1e51795cc61fe78b28a813bf6a730091.js"
 );
 
 self.addEventListener('message', (event) => {
