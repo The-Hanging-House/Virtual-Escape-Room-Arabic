1207e6b81444d9ee24b0ff32bb0762411edd1ebd
clydekarnage <clydeibalez@gmail.com>
2020-09-21 11:06:36 +0400

    email alert1

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,15 +35,17 @@ router.post(
       let user = await User.findOne({ email });
       let user1 = await User.findOne({ username });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Username already exists" }] });
-      }
+      
       if (user1) {
         return res
           .status(400)
           .json({ errors: [{ msg: "Username already exists" }] });
+      }
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Email already exists" }] });
       }
     
 
