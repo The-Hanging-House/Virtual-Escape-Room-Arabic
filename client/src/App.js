32e91561b0c409dba2a35eb9cbce655e3e84b88c
clydekarnage <clydeibalez@gmail.com>
2020-09-22 16:06:26 +0400

    removing some  useless route

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,14 @@ import Dashboard from './components/dashboard/Dashboard';
 import Instruction from './components/dashboard/Instruction';
 import Dashboard2 from './components/dashboard/Dashboard2';
 import Briefing from './components/dashboard/Briefing'
-import Briefing2 from './components/dashboard/Briefing2'
-import Profile from './components/dashboard/Profile';
+// import Briefing2 from './components/dashboard/Briefing2'
+// import Profile from './components/dashboard/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Intro from './components/gameplay/Intro';
 import Scene1 from './components/gameplay/Scene1';
 import Garbage from './components/games/garbage/index';
 import Scene3 from './components/gameplay/Scene3';
-import Portal1Scene3 from './components/gameplay/Portal1Scene3';
+// import Portal1Scene3 from './components/gameplay/Portal1Scene3';
 import Scene4 from './components/gameplay/Scene4';
 import Scene6 from './components/gameplay/Scene6';
 import Scene5 from './components/gameplay/Scene5';
@@ -54,13 +54,13 @@ const App = () => {
               <PrivateRoute exact path="/instruction" component={Instruction} />
               <PrivateRoute exact path="/dashboard2" component={Dashboard2} />
               <PrivateRoute exact path="/briefing" component={Briefing} />
-              <PrivateRoute exact path="/briefing2" component={Briefing2} />
-              <PrivateRoute exact path="/profile" component={Profile} />
+              {/* <PrivateRoute exact path="/briefing2" component={Briefing2} /> */}
+              {/* <PrivateRoute exact path="/profile" component={Profile} /> */}
               <PrivateRoute exact path="/intro" component={Intro} />
               <PrivateRoute exact path="/scene1" component={Scene1} />
               <PrivateRoute exact path="/garbage" component={Garbage} />
               <PrivateRoute exact path="/scene3" component={Scene3} />
-              <PrivateRoute exact path="/portal1scene3" component={Portal1Scene3} />
+              {/* <PrivateRoute exact path="/portal1scene3" component={Portal1Scene3} /> */}
               <PrivateRoute exact path="/scene4" component={Scene4} />
               <PrivateRoute exact path="/scene6" component={Scene6} />
               <PrivateRoute exact path="/scene5" component={Scene5} />
