d846ce32588794f792e15e62625306c644fa3ca8
clydekarnage <clydeibalez@gmail.com>
2020-10-06 17:26:01 +0400

    landing page bg update build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c276b11b2ddef9aee36bdf9e150b7d22.js"
+  "/precache-manifest.506341066d235ccebd91e3009c4a1ea3.js"
 );
 
 self.addEventListener('message', (event) => {
