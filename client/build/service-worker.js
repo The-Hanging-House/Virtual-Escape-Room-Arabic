5584d25694289daf318962f0b29c294a991aad96
siddiq <msm2029@gmail.com>
2021-03-30 13:01:08 +0400

    updated for the arabic version

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce32b444bc6a9947e9b0fe43837c9dd5.js"
+  "/precache-manifest.5074c2e66d3801eb55551533ec343c4d.js"
 );
 
 self.addEventListener('message', (event) => {
