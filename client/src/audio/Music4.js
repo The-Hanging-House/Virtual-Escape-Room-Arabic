303306244b23c1c67bd6fba9359a17560d902bbb
siddiq <msm2029@gmail.com>
2021-05-19 16:08:11 +0400

    safari audio fix

--- client/src/audio/Music4.js
+++ client/src/audio/Music4.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Party from './opening.mp3'
+import React from "react";
+import Party from "./opening.mp3";
 
 class Music1 extends React.Component {
-    render() {
-        return (
-            <div>
-                <audio ref='audio_tag' src={Party} autoPlay loop type="audio/mpeg" />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <audio ref="audio_tag" src={Party} autoPlay loop type="audio/mpeg" />
+      </div>
+    );
+  }
 }
 
 export default Music1;
